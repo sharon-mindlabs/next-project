3d09984f10e0d930166c828c94b014f02716c019
sharon-mindlabs <sharon@mindlabssys.in>
2026-08-07 11:05:23 +0530

    updated blog robot files

--- app/robots.ts
+++ app/robots.ts
@@ -1,3 +1,17 @@
+// import { MetadataRoute } from "next";
+
+// export default function robots(): MetadataRoute.Robots {
+//   return {
+//     rules: {
+//       userAgent: "*",
+//       allow: "/",
+//       disallow: ["/api/", "/admin/", "/login/", "/coming-soon/"],
+//     },
+//     sitemap: "https://flockdesk.com/sitemap.xml",
+//     host: "https://flockdesk.com",
+//   };
+// }
+
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
@@ -7,7 +21,13 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/admin/", "/login/", "/coming-soon/"],
     },
-    sitemap: "https://flockdesk.com/sitemap.xml",
+    sitemap: [
+      "https://flockdesk.com/sitemap.xml",
+      "https://blog.flockdesk.com/post-sitemap.xml",
+      "https://blog.flockdesk.com/page-sitemap.xml",
+      "https://blog.flockdesk.com/category-sitemap.xml",
+      "https://blog.flockdesk.com/author-sitemap.xml",
+    ],
     host: "https://flockdesk.com",
   };
 }
